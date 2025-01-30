a86502aca0855f22fc0b4ae84ebabbaf0bf196a6
pbohea <pbohea@gmail.com>
2025-01-30 02:41:09 +0000

    working

--- app.rb
+++ app.rb
@@ -1,9 +1,29 @@
 require "sinatra"
 require "sinatra/reloader"
 
+
+
+
 get("/") do
   "
   <h1>Welcome to your Sinatra App!</h1>
   <p>Define some routes in app.rb</p>
   "
+end
+
+get("/rock") do
+  
+  
+end
+
+
+get("/paper") do
+
+
+end
+
+
+get("/scissors") do
+
+
 end
